953ebcd892605e9d1dcf3c6bb1cf37a1a7dcef68
purvapatil11 <purvapatil.dev@gmail.com>
2026-03-02 11:42:04 +0530

    completed loader

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ tl.from("#line h1",{
     duration:0.6,
     delay:0.5
 })
-tl.from("#line1-part1 h5, #line1-part1 h6, #line h2",{
+tl.from("#line1-part1 h5, #line1-part1 h6",{
     opacity:0,
     onStart:function () {
         var h5timer = document.querySelector("#line1-part1 h5")
@@ -19,11 +19,25 @@ tl.from("#line1-part1 h5, #line1-part1 h6, #line h2",{
         h5timer.innerHTML = grow
     }
     
-},33)
+},35)
     }
+})
+tl.to("#line h2",{
+    animationName:anime,
+    opacity:1
 })
 tl.to("#loader",{
     opacity:0,
     duration:0.4,
     delay:4
 })
+tl.from("#page1",{
+    delay:0.2,
+    y:1600,
+    opacity:0,
+    ease:Power4,
+    duration:0.5
+})
+tl.to("#loader"),{
+    display:none
+}
