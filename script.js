df68aa45e94eae5adb09a09f3d26a524f695c23d
purvapatil11 <purvapatil.dev@gmail.com>
2026-03-02 16:49:33 +0530

    feat: add custom cursor interaction

--- script.js
+++ script.js
@@ -1,43 +1,56 @@
-var tl = gsap.timeline()
-tl.from("#line h1",{
-    y:150,
-    stagger:0.25,
-    duration:0.6,
-    delay:0.5
-})
-tl.from("#line1-part1 h5, #line1-part1 h6",{
-    opacity:0,
-    onStart:function () {
-        var h5timer = document.querySelector("#line1-part1 h5")
-        var grow = 0
-        setInterval(function(){
-    if(grow<100){
-    grow++
-    h5timer.innerHTML = grow++
-    console.log(grow)  
-    } else{
-        h5timer.innerHTML = grow
-    }
-    
-},35)
-    }
-})
-tl.to("#line h2",{
-    animationName:anime,
-    opacity:1
-})
-tl.to("#loader",{
-    opacity:0,
-    duration:0.4,
-    delay:4
-})
-tl.from("#page1",{
-    delay:0.2,
-    y:1600,
-    opacity:0,
-    ease:Power4,
-    duration:0.5
-})
-tl.to("#loader"),{
-    display:none
+function loadingAnimation(){
+    var tl = gsap.timeline()
+
+    tl.from("#line h1",{
+        y:150,
+        stagger:0.25,
+        duration:0.6,
+        delay:0.5
+    })
+
+    tl.from("#line1-part1 h5, #line1-part1 h6",{
+        opacity:0,
+        onStart:function () {
+            var h5timer = document.querySelector("#line1-part1 h5")
+            var grow = 0
+            setInterval(function(){
+                if(grow < 100){
+                    grow++
+                    h5timer.innerHTML = grow
+                } else{
+                    h5timer.innerHTML = grow
+                }
+            },35)
+        }
+    })
+
+    tl.to("#line h2",{
+        opacity:1
+    })
+
+    tl.to("#loader",{
+        opacity:0,
+        duration:0.4,
+        delay:4
+    })
+
+    tl.from("#page1",{
+        delay:0.2,
+        y:1600,
+        opacity:0,
+        ease: Power4,
+        duration:0.5
+    })
+    tl.to("#loader",{
+        display: "none"
+    })
 }
+
+loadingAnimation()
+
+document.addEventListener("mousemove",function(dets){
+    gsap.to(".crsr",{
+        left:dets.x,
+        top:dets.y
+    })
+})
